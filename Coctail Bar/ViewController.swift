2aeebc7cd510e0d02c2969a880cbca6abdff98b3
Bokhulenkov <bokhulenkov@gmail.com>
2024-06-25 12:20:50 +0300

    Initial Commit

---
+++ Coctail Bar/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Coctail Bar
+//
+//  Created by Alexander Bokhulenkov on 25.06.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .white
+    }
+
+
+}
+
